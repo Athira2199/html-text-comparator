0da1cd16087a7019dc2634c47cf61ea49d4172f9
Athira.V.Ajit <mailathiravajit@gmail.com>
2020-10-03 17:16:36 +0530

    adding improvements in js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const compareStrings = ()=>{
     const str2 = $("#str2").val()
     let s1 = ""
     let s2 = ""
-    for(let i=0;i<str1.length && i<str2.length;i++){
+    for(var i=0;i<str1.length && i<str2.length;i++){
         if(str1[i] == str2[i]){
             s1 += str1[i]
             s2 += str2[i]
@@ -13,5 +13,14 @@ const compareStrings = ()=>{
             s2 +=`<em>${str2[i]}</em>`
         }
     }
+    while(str1.length > i){
+        s1 +=`<em>${str1[i]}</em>`
+        i += 1
+    }
+    while(str2.length > i){
+        s2 +=`<em>${str2[i]}</em>`
+        i += 1
+    }
+    console.log(i)
     $("#result").html(`${s1}<br>${s2}`)
 }
